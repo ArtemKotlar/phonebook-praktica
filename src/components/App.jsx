72ccffc4292a53ed125dbafd99b71327983733f8
ArtemKotlar <artemkotlar93@gmail.com>
2023-02-01 04:59:03 +0200

    початок

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,33 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  addContactToContactBook = value => {
+    const newContact = { id: nanoid(), name: value.name, number: value.number };
+    // { name, number, id: nanoid() } - добавление нового контакта в массив обьектов
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, newContact],
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
